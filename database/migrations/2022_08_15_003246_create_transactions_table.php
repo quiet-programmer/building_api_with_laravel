57481b7c8eaaab5edd1901ca21ab48b3ae2734ea
Godsend Joseph <bobbyjoseph108@gmail.com>
2022-09-08 19:37:39 +0100

    transation table

---
+++ database/migrations/2022_08_15_003246_create_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('tr_ref');
+            $table->string('paystack_ref');
+            $table->double('amount');
+            $table->double('status');
+            $table->foreignId('user_id')->index();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
