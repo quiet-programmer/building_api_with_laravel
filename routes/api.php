e720c8c9c9909f6c57f501cc6f7539b4adfde425
Godsend Joseph <bobbyjoseph108@gmail.com>
2022-10-13 14:27:08 +0100

    lost track

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\API\Note\NoteAuthController;
+use App\Http\Controllers\API\Note\NoteController;
+use App\Http\Controllers\Test\AuthController;
+use App\Http\Controllers\Test\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +20,57 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(AuthController::class)->group(function() {
+    Route::post('register', 'register');
+    Route::post('login', 'login');
+});
+
+Route::middleware('auth:sanctum')->group(function() {
+    // Product route starts here
+    Route::controller(ProductController::class)->group(function() {
+        Route::post('/add_products', 'storeProducts')->name('product.store');
+    });
+    // ends here
+
+    // auth route starts here
+    Route::controller(AuthController::class)->group(function() {
+        Route::post('/logout', 'logout');
+
+        Route::get('/user', 'getUser');
+    });
+    // ends here
+});
+
+Route::controller(ProductController::class)->group(function() {
+    Route::get('/products', 'allProducts')->name('product.all');
+    Route::get('/products/search_product/{name}', 'searchProduct')->name('product.search');
+});
+
+// Note route
+Route::controller(NoteAuthController::class)->group(function() {
+    Route::post('/note/register', 'register');
+    Route::post('/note/login', 'login');
+});
+
+Route::middleware('auth:sanctum')->group(function() {
+
+    // Auth Route
+    Route::controller(NoteAuthController::class)->group(function() {
+        Route::get('/note/user', 'userInfo');
+        Route::get('/note/check_plan', 'checkUserPlan');
+        Route::post('/note/create_plan', 'createPlan');
+        Route::post('/note/logout', 'logout');
+    });
+    // ends here
+
+     // note routes
+    Route::controller(NoteController::class)->group(function() {
+        Route::get('notes/all_notes', 'allNotes');
+        Route::post('notes/add_note', 'addNote');
+        Route::put('notes/update_note/{id}', 'updateNote');
+        Route::delete('notes/delete_note/{id}', 'deleteNote');
+    });
+    // ends here
 });
